79cf64b31d14fcd2216320fe611f2424aaa4cd97
thitima1012 <thitima1235pim@gmail.com>
2026-03-23 15:56:51 +0700

    frontend deploy

--- frontend/src/store/useStore.js
+++ frontend/src/store/useStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API = "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL;
 
 export const useStore = create((set, get) => ({
   token: localStorage.getItem("token"),
