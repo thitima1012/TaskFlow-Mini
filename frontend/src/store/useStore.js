39eca77e38987edf64cc39ee99fb8e7d5e5142f7
thitima1012 <thitima1235pim@gmail.com>
2026-03-23 16:07:20 +0700

    frontend deploy

--- frontend/src/store/useStore.js
+++ frontend/src/store/useStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL;
+const API = "https://taskflow-mini-7k89.onrender.com";
 
 export const useStore = create((set, get) => ({
   token: localStorage.getItem("token"),
